50782ba954ad420968a84d419ce894a2448cf3c7
Benjamin <jijileeeeeee@gmail.com>
2019-08-21 23:34:04 +0800

    add post handling

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const port = 2121;
 
 app.get('/', (req, res) => res.send('Hello world'));
 
+app.post('/user', (req, res) => res.send('Wow, this is working?!!! OH MY GOD!!!'));
+
 app.listen(port, () => console.log(`The app is running and listening on ${port}`));
